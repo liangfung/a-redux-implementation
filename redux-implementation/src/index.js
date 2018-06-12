fb772ee7750f73c83b8235ebe77f5d3dad8ec695
fjl冯觉良 <jl_feng@ctrip.com>
2018-06-12 19:22:40 +0800

    createstore

--- redux-implementation/src/index.js
+++ redux-implementation/src/index.js
@@ -1,8 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+const appState = {
+  title: {
+    text: '再造一个redux',
+    color: 'red'
+  },
+  content: {
+    text: 'content',
+    color: 'blue'
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function createStore(state, stateChanger) {
+  const listeners = []
+  const getState = () => state; // 返回state
+  // 每次dispatch之后，数据变化，要重新render DOM,所以加个subscribe函数，可以手动监听dispatch
+  const subscribe = listener => listeners.push(listener)
+  const dispatch = action => {
+    stateChanger(action); // dispatch，改变数据的唯一方法
+    listeners.forEach(listener => listener())
+  }
+  return {
+    getState,
+    dispatch,
+    subscribe
+  }
+}
+
+function stateChanger(action) {
+  switch(action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      appState.title.text = action.text
+      break;
+    case 'UPDATE_TITLE_COLOR':
+      appState.title.color = action.color
+      break;
+    default:
+      break;
+  }
+}
+
+
+function renderApp(appState) {
+  renderTitle(appState.title);
+  renderContent(appState.content);
+}
+
+function renderTitle(title) {
+  const titleDOM = document.getElementById('title');
+  titleDOM.innerHTML = title.text;
+  titleDOM.style.color = title.color;
+}
+
+function renderContent(content) {
+  const contentDOM = document.getElementById('content');
+  contentDOM.innerHTML = content.text;
+  contentDOM.innerHTML = content.color;
+}
+
+const store = createStore(appState, stateChanger)
+
+renderApp(store.getState());
+store.subscribe(()=>{renderApp(store.getState())}); // 监听dispatch,如果dispatch了，就是state改变了，就执行回调。这里的回调是renderApp
+setTimeout(()=>{store.dispatch({type: 'UPDATE_TITLE_TEXT', text: '修改好了'})},1500)
+setTimeout(()=>{store.dispatch({type: 'UPDATE_TITLE_TEXT', text: '再改一次'})},3000)
--- remark.md
+++ remark.md
@@ -2,3 +2,19 @@
 
 ## 从react的context说起
 在一个应用中，比如主题颜色这里的状态，因为所有子组件都要拿到，所以往往会提升到根组件。但是，状态提升之后还是要一层一层的props传递下去。如果组件的层级比较深的话，维护会变得非常的麻烦。那么，就需要一个方案，让所所有的组件都能拿到，不用一层层的手动传递。
+
+## 提高修改数据的门槛
+app的数据在被多个函数调用之后，可能会发生意想不到的变化。需要提高修改数据的门槛，专门用一个函数来修改数据，也只能用这个函数来修改数据。这个函数叫`dispatch`,`dispatch`接收一个对象作为参数，这个对象用于明确改变方式和转递payload，叫`action`
+
+## 抽象dispatch
+将state和dispath抽象，放在一个store里。使用`createStore`返回一个`store`，用三个函数`getState`,`dispatch`,`subscribe`函数，用于返回state，修改state和监听state的修改并调用回调。
+
+```js
+function createStore(reducer) {
+  const getState = () => state;
+  const listeners = [];
+  const dispatch = reducer => {
+
+  }
+}
+```
