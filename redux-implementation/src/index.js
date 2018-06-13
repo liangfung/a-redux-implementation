32fc3fa9fe8c2e31c82427de16d4a602907a21c1
fjl冯觉良 <jl_feng@ctrip.com>
2018-06-13 16:19:21 +0800

    简陋的diff

--- redux-implementation/src/index.js
+++ redux-implementation/src/index.js
@@ -15,7 +15,7 @@ function createStore(state, stateChanger) {
   // 每次dispatch之后，数据变化，要重新render DOM,所以加个subscribe函数，可以手动监听dispatch
   const subscribe = listener => listeners.push(listener)
   const dispatch = action => {
-    stateChanger(action); // dispatch，改变数据的唯一方法
+    state = stateChanger(state, action); // dispatch，改变数据的唯一方法
     listeners.forEach(listener => listener())
   }
   return {
@@ -25,40 +25,61 @@ function createStore(state, stateChanger) {
   }
 }
 
-function stateChanger(action) {
+function stateChanger(state, action) {
   switch(action.type) {
     case 'UPDATE_TITLE_TEXT':
-      appState.title.text = action.text
-      break;
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          text: action.text
+        }
+      }
     case 'UPDATE_TITLE_COLOR':
-      appState.title.color = action.color
-      break;
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          color: action.color
+        }
+      }
     default:
-      break;
+      return state;
   }
 }
 
 
-function renderApp(appState) {
-  renderTitle(appState.title);
-  renderContent(appState.content);
+function renderApp(newState, oldState = {}) {
+  if (newState === oldState) return;
+  console.log('render app...')
+  renderTitle(newState.title, oldState.title);
+  renderContent(newState.content, oldState.content);
 }
 
-function renderTitle(title) {
+function renderTitle(title, oldTitle = {}) {
+  if (title === oldTitle) return;
+  console.log('render title...')
   const titleDOM = document.getElementById('title');
   titleDOM.innerHTML = title.text;
   titleDOM.style.color = title.color;
 }
 
-function renderContent(content) {
+function renderContent(content, oldContent = {}) {
+  if (content === oldContent) return;
+  console.log('render content...')
   const contentDOM = document.getElementById('content');
   contentDOM.innerHTML = content.text;
   contentDOM.innerHTML = content.color;
 }
 
 const store = createStore(appState, stateChanger)
-
+let oldState = store.getState();
 renderApp(store.getState());
-store.subscribe(()=>{renderApp(store.getState())}); // 监听dispatch,如果dispatch了，就是state改变了，就执行回调。这里的回调是renderApp
+store.subscribe(()=>console.log(1))
+store.subscribe(()=>{
+  const newState = store.getState()
+  renderApp(newState, oldState)
+  oldState = newState
+}); // 监听dispatch,如果dispatch了，就是state改变了，就执行回调。这里的回调是renderApp
 setTimeout(()=>{store.dispatch({type: 'UPDATE_TITLE_TEXT', text: '修改好了'})},1500)
 setTimeout(()=>{store.dispatch({type: 'UPDATE_TITLE_TEXT', text: '再改一次'})},3000)
