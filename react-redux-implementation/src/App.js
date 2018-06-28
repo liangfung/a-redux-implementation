c0d99eb9fa8d26bbf9cda17e9912cdde863ec39b
fjl冯觉良 <jl_feng@ctrip.com>
2018-06-28 16:30:23 +0800

    通过context共享store,分别在组件中拿到store，getState，dispat和subscribe update回调

--- react-redux-implementation/src/App.js
+++ react-redux-implementation/src/App.js
@@ -1,16 +1,67 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import Header from './Header'
+import Content from './Content'
 
+function createStore(reducer) {
+  let state;
+  let listeners = []
+  const getState = () => state;
+  const dispatch = action => {
+    state = reducer(state, action)
+    listeners.forEach(listener => listener())
+  }
+  const subscribe = listener => {
+    listeners.push(listener)
+  }
+  dispatch({})
+  return {
+    getState,
+    dispatch,
+    subscribe
+  }
+}
+
+const themeReducer = (state, action) => {
+  const initialState = {
+    themeColor: 'red'
+  }
+  if (!state) {
+    return initialState;
+  }
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return {
+        ...state,
+        themeColor: action.themeColor
+      }
+    default:
+      return state
+  }
+}
+
+const store = createStore(themeReducer)
 class App extends Component {
+  static childContextTypes = {
+    store: PropTypes.object
+  }
+
+  constructor() {
+    super()
+    this.state = {
+      themeColor: 'red'
+    }
+  }
+
+  getChildContext() {
+    return { store }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Content />
       </div>
     );
   }
---
+++ react-redux-implementation/src/Content.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import ThemeSwitch from './ThemeSwitch'
+import PropTypes from 'prop-types'
+
+class Content extends Component {
+  static contextTypes = {
+    store: PropTypes.object
+  }
+
+  state = {
+    themeColor: ''
+  }
+
+  componentDidMount() {
+    const { store } = this.context
+    this._updateThemeColor()
+    store.subscribe(this._updateThemeColor)
+  }
+
+  _updateThemeColor = () => {
+    const store = this.context.store
+    const state = store.getState()
+    this.setState({ themeColor: state.themeColor })
+  }
+
+  render() {
+    return (
+      <div style={{ color: this.state.themeColor }}>
+        <p>这是内容的部分</p>
+        <ThemeSwitch />
+      </div>
+    )
+  }
+}
+
+export default Content
