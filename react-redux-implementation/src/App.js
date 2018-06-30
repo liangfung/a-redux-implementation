169eeb8b59383d1efddd504f9fbf5624d91a9936
liangfung <1098486429@qq.com>
2018-07-01 00:19:35 +0800

    add provider

--- react-redux-implementation/src/App.js
+++ react-redux-implementation/src/App.js
@@ -1,26 +1,8 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'
 import Header from './Header'
 import Content from './Content'
-
-function createStore(reducer) {
-  let state;
-  let listeners = []
-  const getState = () => state;
-  const dispatch = action => {
-    state = reducer(state, action)
-    listeners.forEach(listener => listener())
-  }
-  const subscribe = listener => {
-    listeners.push(listener)
-  }
-  dispatch({})
-  return {
-    getState,
-    dispatch,
-    subscribe
-  }
-}
+import { Provider } from './react-redux'
+import createStore from './store'
 
 const themeReducer = (state, action) => {
   const initialState = {
@@ -41,29 +23,30 @@ const themeReducer = (state, action) => {
 }
 
 const store = createStore(themeReducer)
+
 class App extends Component {
-  static childContextTypes = {
-    store: PropTypes.object
-  }
+  // static childContextTypes = {
+  //   store: PropTypes.object
+  // }
 
-  constructor() {
-    super()
-    this.state = {
-      themeColor: 'red'
-    }
-  }
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     themeColor: 'red'
+  //   }
+  // }
 
-  getChildContext() {
-    return { store }
-  }
+  // getChildContext() {
+  //   return { store }
+  // }
 
   render() {
     return (
-      <div className="App">
+      <Provider store={store}>
         <Header />
         <Content />
-      </div>
-    );
+      </Provider>
+    )
   }
 }
 
---
+++ react-redux-implementation/src/store.js
@@ -0,0 +1,18 @@
+export default function createStore(reducer) {
+  let state;
+  let listeners = []
+  const getState = () => state;
+  const dispatch = action => {
+    state = reducer(state, action)
+    listeners.forEach(listener => listener())
+  }
+  const subscribe = listener => {
+    listeners.push(listener)
+  }
+  dispatch({})
+  return {
+    getState,
+    dispatch,
+    subscribe
+  }
+}
